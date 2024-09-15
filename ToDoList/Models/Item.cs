6ca2e54a35ac1e264b0f754001de3f8dc9156c68
omhirvania123 <150777776+omhirvania123@users.noreply.github.com>
2024-09-15 13:01:17 +0530

    Add files via upload

---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace ToDoList.Models
+{
+    public class Item
+    {
+        public Item()
+        {
+            this.Categories = new HashSet<CategoryItem>();
+        }
+
+        public int ItemId { get; set; }
+        public string Description { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+        public ICollection<CategoryItem> Categories { get;}
+    }
+}
